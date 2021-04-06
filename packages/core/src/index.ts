7d4c9482dbd457e40b65751fd6c6d0cd173f3647
Tom <quiibzdev@gmail.com>
2021-04-06 13:55:53 +0200

    chore: Add tsdoc to types

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,8 +1,15 @@
 import * as CSS from 'csstype';
 
+// The type of all CSS keys
 export type CSSKeys = keyof CSS.Properties;
+
+// The type of all CSS values
 export type CSSValues = CSS.Properties[CSSKeys];
 
+/**
+ * A type representing additional CSS features. For example, the `important` field represent
+ * the `!important` in CSS.
+ */
 export type Features = {
     value: CSSValues,
     important: boolean,
@@ -53,11 +60,15 @@ export type Style<T> = {
 export type ClassProvider = string[];
 
 /**
- * A type representing a function which provides css properties with a callback or an object
- * of css properties.
+ * A type representing a function which provides css properties with a callback or an
+ * object of css properties.
  */
 export type StyleProvider<T> = ((theme: NiftyTheme<T>) => CSSProperties) | CSSProperties;
 
+/**
+ * A type reprensing a function with provides a new theme given the old theme, or the new
+ * theme directly.
+ */
 export type ThemeProvider<T> = ((theme: NiftyTheme<T>) => NiftyTheme<T>) | NiftyTheme<T>;
 
 export { default as Nifty } from './Nifty';
