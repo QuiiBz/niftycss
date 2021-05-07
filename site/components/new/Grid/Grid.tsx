59677fb64254c718a2d11af37d397598907e56f1
Tom <quiibzdev@gmail.com>
2021-05-07 09:04:06 +0200

    refactor: Responsive home page

--- site/components/new/Grid/Grid.tsx
+++ site/components/new/Grid/Grid.tsx
@@ -1,21 +1,29 @@
-import { calc } from '@niftycss/css';
+import { calc, flexColumn, paddingX } from '@niftycss/css';
 import { HEADER_HEIGHT } from '../../../lib/constants';
 import { styled } from '../../../lib/nifty';
 
 const Grid = styled('div', {
-    height: calc`100vh - ${HEADER_HEIGHT}`,
-    display: 'grid',
-    gridTemplateColumns: '1fr minmax(340px, 1fr) minmax(340px, 1fr) minmax(340px, 1fr) 1fr',
-    gridTemplateRows: '1fr 1fr',
+    ...flexColumn,
+    ...paddingX`40px`,
+    alignItems: 'center',
+    height: 'auto',
     position: 'relative',
     zIndex: 1,
-    // @ts-ignore
-    '> div:not(:nth-child(-n+2))': {
-        borderLeft: '2px dashed @gray400',
-        borderImage: 'linear-gradient(to bottom, transparent, @gray400 30%) 2',
-    },
-    '> div:last-child': {
-        borderRight: '2px dashed @gray400',
+    $lg: {
+        ...paddingX`0`,
+        display: 'grid',
+        alignItems: 'initial',
+        gridTemplateColumns: '1fr minmax(340px, 1fr) minmax(340px, 1fr) minmax(340px, 1fr) 1fr',
+        gridTemplateRows: '1fr 1fr',
+        height: calc`100vh - ${HEADER_HEIGHT}`,
+        // @ts-ignore
+        '> div:not(:nth-child(-n+2))': {
+            borderLeft: '2px dashed @gray400',
+            borderImage: 'linear-gradient(to bottom, transparent, @gray400 30%) 2',
+        },
+        '> div:last-child': {
+            borderRight: '2px dashed @gray400',
+        },
     },
 });
 
--- site/components/styles/Container.ts
+++ site/components/styles/Container.ts
@@ -3,7 +3,10 @@ import { styled } from '../../lib/nifty';
 
 const Container = styled('div', {
     ...marginX`auto`,
-    width: 'max(60vw, calc(340px * 3))',
+    width: 'max(80vw, 340px)',
+    $lg: {
+        width: 'max(60vw, calc(340px * 3))',
+    },
 });
 
 export default Container;
